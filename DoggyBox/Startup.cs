a416be26f0d0dd3198ff2896092558cc85f80c89
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-11-05 23:22:37 +0200

    initial

---
+++ DoggyBox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoggyBox.Startup))]
+namespace DoggyBox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
