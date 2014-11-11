ad1e890d5c850261d9522248add4beb7d52b5161
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-11-11 18:32:50 +0200

    base controller added

---
+++ DoggyBox.Web/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿namespace DoggyBox.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using DoggyBox.Data.UnitsOfWork;
+    using DoggyBox.Data.Contracts;
+
+    public class BaseController : Controller
+    {
+
+        public BaseController()
+            : this(new EFDataProvider())
+        {
+        }
+
+        public BaseController(IDataProvider data)
+        {
+            this.Data = data;
+        }
+
+        protected IDataProvider Data { get; set; }
+    }
+}
