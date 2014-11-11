8b96e24d9d3d6c6a4746f5abb31ac0d8552398bd
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-11-11 17:44:29 +0200

    context creating tables

--- DoggyBox.Data/ApplicationDbContext.cs
+++ DoggyBox.Data/ApplicationDbContext.cs
@@ -16,5 +16,11 @@
         {
             return new ApplicationDbContext();
         }
+
+        public IDbSet<Dog> Dogs { get; set; }
+        public IDbSet<Trainer> Trainers { get; set; }
+        public IDbSet<Course> Courses { get; set; }
+        public IDbSet<Practice> Practices { get; set; }
+        public IDbSet<Mark> Marks { get; set; }
     }
 }
