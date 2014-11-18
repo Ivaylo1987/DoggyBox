5675c35075a751ae7602ec19e1a863c3b08fba97
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-11-18 18:52:53 +0200

    administration changes

---
+++ DoggyBox.Web/Areas/Administration/Models/CourseViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DoggyBox.Web.Areas.Administration.Models
+{
+    using Application.Models.Base;
+    using DoggyBox.Infrastructure.AutoMapper;
+    using DoggyBox.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class CourseViewModel : AdministrationViewModel, IMapFrom<Course>
+    {
+        public int Id { get; set; }
+
+    }
+}
